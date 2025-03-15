6ca3dd83a47b43eb00398869115af7e8a7172d9c
Okun Sergey <OkunSA@yandex.ru>
2025-03-15 16:59:07 +0300

    Codestyle bagfix

--- src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
+++ src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
@@ -2,17 +2,11 @@ package ru.yandex.practicum.filmorate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import ru.yandex.practicum.filmorate.model.Film;
-
-import java.time.LocalDate;
 
 @SpringBootApplication
 public class FilmorateApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(FilmorateApplication.class, args);
-
-		// TODO Добавьте логирование для операций, которые изменяют сущности — добавляют и обновляют их.
-		//  Также логируйте причины ошибок — например, если валидация не пройдена. Это считается хорошей практикой.
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FilmorateApplication.class, args);
+    }
 
 }
